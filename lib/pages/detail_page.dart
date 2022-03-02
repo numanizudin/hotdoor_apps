be98fa4b1801d79f863de15f92eed4cd51a72c8b
numanizudin <muhammadnumanizudin@gmail.com>
2022-03-02 11:43:05 +0700

    detail page 3

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hotdoor_apps/theme.dart';
+import 'package:hotdoor_apps/widgets/facility_item.dart';
 
 class DetailPage extends StatelessWidget {
   @override
@@ -53,10 +54,12 @@ class DetailPage extends StatelessWidget {
                     color: whiteColor,
                   ),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       SizedBox(
                         height: 30,
                       ),
+                      // NOTE: TITLE
                       Padding(
                         padding: EdgeInsets.symmetric(
                           horizontal: edge,
@@ -130,6 +133,47 @@ class DetailPage extends StatelessWidget {
                                   color: Color(0xff989BA1),
                                 ),
                               ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      // NOTE: MAIN FACILITIES
+                      Padding(
+                        padding: EdgeInsets.only(left: edge),
+                        child: Text(
+                          'Main Facilities',
+                          style: regularTextStyle.copyWith(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: edge,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            FacilityItem(
+                              name: 'Kitchen',
+                              imageUrl: 'assets/icon_kitchen.png',
+                              total: 2,
+                            ),
+                            FacilityItem(
+                              name: 'Bedroom',
+                              imageUrl: 'assets/icon_bedroom.png',
+                              total: 3,
+                            ),
+                            FacilityItem(
+                              name: 'Big Lemari',
+                              imageUrl: 'assets/icon_cupboard.png',
+                              total: 3,
                             ),
                           ],
                         ),
